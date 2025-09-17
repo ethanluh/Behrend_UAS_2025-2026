c6b714292a54bb055be03f8d559084242cc199bd
Ethan <ethanluh@gmail.com>
2025-09-16 22:59:05 -0400

    Basic camera

--- Computer Vision/camera.py
+++ Computer Vision/camera.py
@@ -2,19 +2,11 @@ import cv2
 
 cam = cv2.VideoCapture(0)
 
-frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (frame_width, frame_height))
-
 while True:
     ret, frame = cam.read()
-    out.write(frame)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cam.release()
-out.release()
 cv2.destroyAllWindows()
